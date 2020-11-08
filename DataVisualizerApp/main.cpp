0d506c47406e9de3760ec4b9a32113ffd960e29a
Luka Čubrilo <lcubrilo01@gmail.com>
2020-11-08 14:39:38 +0100

    Logged into wolk thru curl the sloppy way

--- DataVisualizerApp/main.cpp
+++ DataVisualizerApp/main.cpp
@@ -2,6 +2,9 @@
 #include <curl/curl.h>
 #include <QApplication>
 #include <iostream>
+#include <QJsonObject>
+#include <QJsonDocument>
+
 
 struct MemoryStruct {
   char *memory;
@@ -32,27 +35,6 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
     return realsize;
 }
 
-char *readToken(CURL *c)
-{
-    MemoryStruct chunk;
-    chunk.memory = new char(0);
-    chunk.size = 0;
-    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-    curl_easy_setopt(c, CURLOPT_WRITEDATA, (void *)&chunk);
-    std::cout<<curl_easy_perform(c);
-    return chunk.memory;
-}
-
-char *loginWolk(CURL *c)
-{
-    c = curl_easy_init();
-    curl_easy_setopt(c, CURLOPT_URL, "https://api-demo.wolkabout.com/api/emailSignIn/");
-    curl_easy_setopt(c, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_easy_setopt(c, CURLOPT_POSTFIELDS, "{\"username\": \"lcubrilo01@gmail.com\",\"password\": \"FHp4LwCzGNXchS2\"}");
-    std::cout<<curl_easy_perform(c);
-    //return readToken(c);
-}
-
 void getDevice(CURL *c, char* token)
 {
     curl_easy_setopt(c, CURLOPT_URL, "https://api-demo.wolkabout.com/api/devices/471d6850-8bed-4eca-9e6e-97fd9300b117");
@@ -66,15 +48,54 @@ void getDevice(CURL *c, char* token)
     curl_easy_perform(c);
 }
 
+char * loginWolk(CURL *c)
+{
+    //URL, request type
+    curl_easy_setopt(c, CURLOPT_URL, "https://api-demo.wolkabout.com/api/emailSignIn/");
+    curl_easy_setopt(c, CURLOPT_CUSTOMREQUEST, "POST");
+
+    //body
+    /*QJsonObject obj;
+    obj.insert("username","lcubrilo01@gmail.com");
+    obj.insert("password","FHp4LwCzGNXchS2");
+    QJsonDocument doc(obj);
+    std::string jsonString = doc.toJson(QJsonDocument::Compact).toStdString();*/
+    curl_easy_setopt(c, CURLOPT_POSTFIELDS, "{\"username\":\"lcubrilo01@gmail.com\",\"password\":\"FHp4LwCzGNXchS2\"}");
+
+    //header
+    struct curl_slist* header = NULL;
+    header = curl_slist_append(header,"Content-Type: application/json");
+    curl_easy_setopt(c, CURLOPT_HTTPHEADER, header);
+
+    //preparing for response
+    MemoryStruct chunk;
+    chunk.memory = new char(0);
+    chunk.size = 0;
+    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(c, CURLOPT_WRITEDATA, (void *)&chunk);
+
+    //performing
+    CURLcode code = curl_easy_perform(c);
+
+    //get token from response
+    /*doc = QJsonDocument::fromJson(chunk.memory);
+    obj = doc.object();
+    QJsonValue token = obj.take("accessToken");
+    return token.toString();*/
+    return chunk.memory;
+}
+
 int main(int argc, char *argv[])
 {
-    CURL* c;
-   //char* token =
-            loginWolk(c);
-    //getDevice(c, token);
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    CURL* c = curl_easy_init();
+    std::cout<<loginWolk(c);
+    //QString token = loginWolk(c);
+    //std::cout<<token.toUtf8().constData();
+    //getDevice(c, token);
+
     return a.exec();
 }
--- DataVisualizerApp/mainwindow.cpp
+++ DataVisualizerApp/mainwindow.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+/*QString promeni(int i)
+{
+    QString str = ui->textEdit->toPlainText();
+    int x = str.toInt() + i;
+    str = QString(x);
+    return str;
+}*/
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    /*connect(ui->pushButton_2,
+            SIGNAL(clicked()),
+            ui->textEdit,
+            SLOT(setPlainText(QString(ui->textEdit->toPlainText().toInt() + 1)))
+            );
+    connect(ui->pushButton_3,
+            SIGNAL(clicked()),
+            ui->textEdit,
+            SLOT(setPlainText(QString(ui->textEdit->toPlainText().toInt() - 1)))
+            );*/
 }
 
 MainWindow::~MainWindow()
