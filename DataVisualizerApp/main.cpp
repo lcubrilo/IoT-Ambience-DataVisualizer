af8744dca438424a4a841659ecb2b33ef306bbd5
Luka Čubrilo <lcubrilo01@gmail.com>
2020-11-09 01:34:35 +0100

    Made the writecallback and parsing way more elegant, fixed the login, tossed out useless code. getdevice still not working

--- DataVisualizerApp/main.cpp
+++ DataVisualizerApp/main.cpp
@@ -6,83 +6,74 @@
 #include <QJsonDocument>
 
 
-struct MemoryStruct {
-  char *memory;
-  size_t size;
-};
-
-static size_t
-WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+std::string parseValue(std::string json, std::string lookingFor)
 {
-    size_t realsize = size * nmemb;
-    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+    int start = json.find(lookingFor) + lookingFor.length() + 3;
+    int end = json.find("\"", start);
+    int len = end - start;
 
-    //prebaceno u c++
-    char *ptr = new char(mem->size + realsize + 1);
-    ptr = mem->memory;
-
-  /*char *ptr = realloc(mem->memory, mem->size + realsize + 1); //ovo je C
-    if(ptr == NULL) {
-        //out of memory!
-        printf("not enough memory (realloc returned NULL)\n");
-    return 0;
-  }*/
-    mem->memory = ptr;
-
-    memcpy(&(mem->memory[mem->size]), contents, realsize);
-    mem->size += realsize;
-    mem->memory[mem->size] = 0;
-    return realsize;
+    return json.substr(start, len);
 }
 
-void getDevice(CURL *c, char* token)
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+void  getDevice(CURL *c, std::string token)
+{
+    //URL, request type
     curl_easy_setopt(c, CURLOPT_URL, "https://api-demo.wolkabout.com/api/devices/471d6850-8bed-4eca-9e6e-97fd9300b117");
     curl_easy_setopt(c, CURLOPT_CUSTOMREQUEST, "GET");
 
+    //setting up the header (authorization)
     struct curl_slist* header = NULL;
-    header = curl_slist_append(header, "Authorization: Bearer");
-    header = curl_slist_append(header, token);
-
+    header = curl_slist_append(header, "Authorization: Bearer ");
+    header = curl_slist_append(header, &token[0]);
     curl_easy_setopt(c, CURLOPT_HTTPHEADER, header);
-    curl_easy_perform(c);
-}
 
-char * loginWolk(CURL *c)
+    //prepare for response
+    std::string received;
+    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(c, CURLOPT_WRITEDATA, &received);
+
+    //performing
+    curl_easy_perform(c);
+
+    //parse device name out of response
+    std::string deviceName = "lol";
+    deviceName+=parseValue(received, "name");
+    std::cout<<deviceName;
+}
+std::string loginWolk(CURL *c)
 {
     //URL, request type
     curl_easy_setopt(c, CURLOPT_URL, "https://api-demo.wolkabout.com/api/emailSignIn/");
     curl_easy_setopt(c, CURLOPT_CUSTOMREQUEST, "POST");
 
-    //body
-    /*QJsonObject obj;
+    //body; string -> object -> document -> json
+    QJsonObject obj;
     obj.insert("username","lcubrilo01@gmail.com");
     obj.insert("password","FHp4LwCzGNXchS2");
     QJsonDocument doc(obj);
-    std::string jsonString = doc.toJson(QJsonDocument::Compact).toStdString();*/
-    curl_easy_setopt(c, CURLOPT_POSTFIELDS, "{\"username\":\"lcubrilo01@gmail.com\",\"password\":\"FHp4LwCzGNXchS2\"}");
+    std::string jsonString = doc.toJson(QJsonDocument::Compact).toStdString();
+    curl_easy_setopt(c, CURLOPT_POSTFIELDS, jsonString.c_str());
 
     //header
     struct curl_slist* header = NULL;
     header = curl_slist_append(header,"Content-Type: application/json");
     curl_easy_setopt(c, CURLOPT_HTTPHEADER, header);
 
-    //preparing for response
-    MemoryStruct chunk;
-    chunk.memory = new char(0);
-    chunk.size = 0;
-    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-    curl_easy_setopt(c, CURLOPT_WRITEDATA, (void *)&chunk);
+    //prepare for response
+    std::string received;
+    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(c, CURLOPT_WRITEDATA, &received);
 
     //performing
-    CURLcode code = curl_easy_perform(c);
+    curl_easy_perform(c);
 
-    //get token from response
-    /*doc = QJsonDocument::fromJson(chunk.memory);
-    obj = doc.object();
-    QJsonValue token = obj.take("accessToken");
-    return token.toString();*/
-    return chunk.memory;
+    //parse token out of response
+    return parseValue(received, "accessToken");
 }
 
 int main(int argc, char *argv[])
@@ -92,10 +83,10 @@ int main(int argc, char *argv[])
     w.show();
 
     CURL* c = curl_easy_init();
-    std::cout<<loginWolk(c);
-    //QString token = loginWolk(c);
-    //std::cout<<token.toUtf8().constData();
-    //getDevice(c, token);
+    std::string accessToken = loginWolk(c);
 
+    getDevice(c, accessToken);
+
+    curl_easy_cleanup(c);
     return a.exec();
 }
