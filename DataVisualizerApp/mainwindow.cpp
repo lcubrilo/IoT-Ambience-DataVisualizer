1668bed00f013d87e7389f970d831a1bedb11efd
Luka Čubrilo <lcubrilo01@gmail.com>
2020-11-10 13:42:12 +0100

    Pretty much everything works properly. You could call it a beta version

--- DataVisualizerApp/mainwindow.cpp
+++ DataVisualizerApp/mainwindow.cpp
@@ -1,10 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+//FHp4LwCzGNXchS2
 QString email;
 QString pass;
 MainWindow* prozor;
-std::string token;
+QString token;
 
 class device
 {
@@ -36,7 +36,7 @@ class device
     }
 };
 
-std::string parseValue(std::string json, std::string lookingFor, int from = 0)
+QString parseValue(std::string json, std::string lookingFor, int from = 0)
 {
     int start = json.find(lookingFor, from) + lookingFor.length() + 3;
     int end = json.find("\"", start);
@@ -50,12 +50,30 @@ std::string parseValue(std::string json, std::string lookingFor, int from = 0)
         value = json.substr(start, len); //redo with the fixed endpoint
     }
 
-    return value;
+    return QString::fromUtf8(value.c_str());
 }
-
 int locateValue(std::string json, std::string lookingFor, int from = 0)
 {
     return json.find(lookingFor, from) + lookingFor.length() + 3;
+}
+QString getSensorValue(std::string json, QString sensor, QString device)
+{
+    int iterator = 0;
+    QString tmpSensor, tmpDevice, value;
+    do //in the proper device
+    {
+        do //iterate until the proper sensor
+        {
+            tmpSensor = parseValue(json, "name", iterator);
+            iterator = locateValue(json, "name", iterator) + 1;
+        }while(tmpSensor!=sensor);
+
+        tmpDevice = parseValue(json, "deviceName", iterator);
+        iterator = locateValue(json, "deviceName", iterator) + 1;
+
+    }while(tmpDevice!=device);
+
+    return parseValue(json, "value", iterator);
 }
 
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
@@ -63,15 +81,16 @@ size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
-void  getDevice()
+std::string get(char* url)
 {
     CURL* c = curl_easy_init();
     //URL, request type
-    curl_easy_setopt(c, CURLOPT_URL, "https://api-demo.wolkabout.com/api/devices/");
+    curl_easy_setopt(c, CURLOPT_URL, url);
     curl_easy_setopt(c, CURLOPT_CUSTOMREQUEST, "GET");
 
     //setting up the header (authorization)
-    std::string authorization = "Authorization: Bearer " + token;
+    std::string authorization = token.toLocal8Bit().constData();
+    authorization = "Authorization: Bearer " + authorization;
     struct curl_slist* headers = NULL;
     headers = curl_slist_append(headers, &authorization[0]);
     curl_easy_setopt(c, CURLOPT_HTTPHEADER, headers);
@@ -83,31 +102,49 @@ void  getDevice()
 
     //performing
     curl_easy_perform(c);
-
-    //parse device name out of response
-    std::string deviceName;
-    int deviceLoc = 0;
-    do
-    {
-        deviceName = parseValue(received, "deviceName", deviceLoc);
-        deviceLoc = locateValue(received, "deviceName", deviceLoc) + 1;
-        prozor->ui->deviceCombo->addItem(QString::fromUtf8(deviceName.c_str()));
-
-
-        int presLoc = locateValue(received,"Barometer", 0);
-        std::string pressure = parseValue(received, "value", presLoc);
-
-        int humiLoc = locateValue(received,"Hygrometer", 0);
-        std::string humidity = parseValue(received, "value", humiLoc);
-
-        int tempLoc = locateValue(received,"Thermometer", 0);
-        std::string temperature = parseValue(received, "value", tempLoc);
-    }
-    while(deviceName!="")
     curl_easy_cleanup(c);
+
+    return received;
 }
 void getDevices()
 {
+    std::string url = "https://api-demo.wolkabout.com/api/devices/";
+    std::string received =  get(&url[0]);
+
+    //parse device name out of response
+    QString deviceName;
+    int deviceLoc = 0;
+
+    do
+    {
+        deviceName = parseValue(received, "deviceName", deviceLoc);
+        deviceLoc = locateValue(received, "deviceName", deviceLoc) + 1; //iterate
+
+        if(deviceName.contains(":")) //there must be none of :
+            break;
+        else if(prozor->ui->deviceCombo->findText(deviceName)==-1) //if its not in the list
+            prozor->ui->deviceCombo->addItem(deviceName);
+    }
+    while(!deviceName.contains(":"));
+}
+void getSensors()
+{
+    std::string url = "https://api-demo.wolkabout.com/api/deviceSensors/";
+    std::string received = get(&url[0]);
+
+    QString device = prozor->ui->deviceCombo->currentText();
+
+    QString temperature = getSensorValue(received, "Thermometer", device);
+    prozor->ui->lcdTemp->display(temperature);
+    qDebug() << temperature;
+
+    QString pressure = getSensorValue(received, "Barometer", device);
+    prozor->ui->lcdPres->display(pressure);
+    qDebug() << pressure;
+
+    QString humidity = getSensorValue(received, "Hygrometer", device);
+    prozor->ui->lcdHumi->display(humidity);
+    qDebug() << humidity;
 
 }
 void loginWolk()
@@ -140,7 +177,7 @@ void loginWolk()
     curl_easy_perform(c);
 
     //parse token out of response
-    std::string str = parseValue(received, "accessToken", 0);
+    QString str = parseValue(received, "accessToken", 0);
     if(str!="")
      {
         prozor->ui->isConnected->setChecked(true);
@@ -166,10 +203,14 @@ MainWindow::MainWindow(QWidget *parent)
     this->ui->isConnected->setAttribute(Qt::WA_TransparentForMouseEvents);
     this->ui->isConnected->setFocusPolicy(Qt::NoFocus);
     prozor = this;
+
     connect(ui->pushButton, &QPushButton::clicked, loginWolk);
     connect(ui->emailLine, &QLineEdit::textChanged, updateEmail);
     connect(ui->passLine, &QLineEdit::textChanged, updatePass);
-    connect(ui->deviceCombo, &QComboBox::currentTextChanged, getDevice);
+
+    connect(ui->refreshButton, &QPushButton::clicked, getDevices);
+    connect(ui->deviceCombo, &QComboBox::currentTextChanged, getSensors);//  highlighted
+    //connect(ui->deviceCombo, &QComboBox::currentTextChanged, getDevice);
 
 }
 
